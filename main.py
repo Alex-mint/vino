89922d5ae8966cf1b86f7101f8586770dfc5aac9
Alex <soyvitaly@gmail.com>
2021-11-08 21:29:43 +0100

    Fixed the code

--- main.py
+++ main.py
@@ -1,64 +1,50 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
+import pandas
+import argparse
 
+from collections import defaultdict
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-import datetime
 
-import pandas
+def create_parser():
+    parser = argparse.ArgumentParser(description='Specify the path to the excel file.')
+    parser.add_argument('-f', '--file', default='wine.xlsx',
+                        help='Path to Excel file')
+    return parser.parse_args()
 
-import collections
 
-from pprint import pprint
+def get_years():
+    today = int(datetime.datetime.now().year)
+    years = today - 1920
+    return years
 
 
-excel_data = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', keep_default_na=False, usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'])
+def get_catalog(file):
+    catalog = pandas.read_excel(file, sheet_name='Лист1', na_values=None,
+                                keep_default_na=False).to_dict(orient='records')
+    categorized_catalog = defaultdict(list)
+    for drink in catalog:
+        categorized_catalog[drink['Категория']].append(drink)
+    return sorted(categorized_catalog.items())
 
-excel_data_dict = excel_data.to_dict(orient='record')
 
-def categorie_catalog(category_name):
-    categorie_catalog = []
-    for dic in excel_data_dict:
-        for key, value in dic.items():
-            if key == 'Категория' and value == category_name:
-                categorie_catalog.append(dic)
-    return categorie_catalog
+def main():
+    args = create_parser()
+    #file = args.file #'wine.xlsx'
+    catalog = get_catalog(args.file)
+    env = Environment(loader=FileSystemLoader('.'),
+                      autoescape=select_autoescape(['html', 'xml']))
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        years=f'Уже {get_years()} лет с вами',
+        catalog=catalog
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-def drinks_catalog():
-    categorie_names = []
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
-    for dic in excel_data_dict:
-        for key, value in dic.items():
-            if key == 'Категория' :
-                categorie_names.append(value)
-    category_names = sorted(set(categorie_names))
-    
-    drinks_catalog = collections.defaultdict(list)
-
-    for category_name in category_names:
-        drinks_catalog[category_name] = categorie_catalog(category_name)
-
-    return drinks_catalog
-
-drinks_catalog = drinks_catalog()
-
-pprint(drinks_catalog)
-
-present_time = datetime.datetime.now()
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-rendered_page = template.render(
-    company_age=present_time.year - 1920,
-    drinks_catalog=drinks_catalog,
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == "__main__":
+    main()
