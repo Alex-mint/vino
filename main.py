69bb798888eef5e36404e9461e84825d3c2cbbf3
Alex <soyvitaly@gmail.com>
2021-11-09 22:38:45 +0100

    Clarified the names of the variables

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+FOUNDATION_YEAR = 1920
+
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='Specify the path to the excel file.')
+    parser = argparse.ArgumentParser(
+        description='Specify the path to the excel file.')
     parser.add_argument('-f', '--file', default='wine.xlsx',
                         help='Path to Excel file')
     return parser.parse_args()
 
 
-def get_years():
-    today = int(datetime.datetime.now().year)
-    years = today - 1920
-    return years
+def get_winery_age():
+    present_year = int(datetime.datetime.now().year)
+    winery_age = present_year - FOUNDATION_YEAR
+    return winery_age
 
 
 def get_catalog(file):
@@ -36,14 +39,16 @@ def main():
                       autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template.html')
     rendered_page = template.render(
-        years=f'Уже {get_years()} лет с вами',
+        years=f'Уже {get_winery_age()} лет с вами',
         catalog=catalog
     )
+
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
+
 
 if __name__ == "__main__":
     main()
